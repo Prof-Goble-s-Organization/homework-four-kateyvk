a937df14db86775332320108860d5cafde0298f1
kateyvk <131284242+kateyvk@users.noreply.github.com>
2024-10-23 21:30:06 -0400

    Finished hw4 using 1 late day late days remaining: 2.

--- hw04/src/hw04/MinKeyFinder.java
+++ hw04/src/hw04/MinKeyFinder.java
@@ -4,15 +4,23 @@ package hw04;
  * Find the key associated with the minimum value in a tree.
  */
 public class MinKeyFinder implements CS232Visitor<String, Integer> {
-
+	private String minKey;
+	private Integer minVal;
 
 	public MinKeyFinder() {
+		minKey = null;
+		minVal = null;
 	}
 
 	/**
 	 * {@inheritDoc}
 	 */
 	public void visit(String key, Integer value) {
+		if(minVal == null||value < minVal) { //if its the first value or if the current val is lower than recordMin adjust the variables
+			minVal = value; //This function will update after each node 
+			minKey = key;
+		}
+		
 
 	}
 
@@ -22,10 +30,17 @@ public class MinKeyFinder implements CS232Visitor<String, Integer> {
 	 * @return the key associated with the minimum value.
 	 */
 	public String getMinKey() {
-		return null;
+		return minKey;
 	}
 	
 	public static void main(String[] args) {
-
+		String[]keys = {"A","B","C","D","E","F","G","H","I","J"};
+		Integer[] vals = {100,90,80,70,60,10,40,30,20,30};
+		 
+		
+		CS232LinkedBinaryTree<String,Integer> testTree = new CS232LinkedBinaryTree<>(keys, vals);
+		MinKeyFinder visitor1 = new MinKeyFinder();
+		testTree.visitPreOrder(visitor1);
+		System.out.println("Min Key:"+ visitor1.getMinKey());
 	}
 }
